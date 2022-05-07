40bbfd69cb53f08f3fe7fc162f494075aa6cdc8d
cjh1249131356 <cjh1249131356@gmail.com>
2022-05-08 00:45:16 +0800

    reTicker: Allow to only show in Landscape mode [1/2]
    * Also use SettingsObserver to observe change.
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5807,6 +5807,12 @@ public final class Settings {
         public static final String RETICKER_COLORED = "reticker_colored";
 
         /**
+         * Only enable reTicker in landscape mode
+         * @hide
+         */
+        public static final String RETICKER_LANDSCAPE_ONLY = "reticker_landscape_only";
+
+        /**
          * @hide
          */
         public static final String USE_OLD_MOBILETYPE = "use_old_mobiletype";
