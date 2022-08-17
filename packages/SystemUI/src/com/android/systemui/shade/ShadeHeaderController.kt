615807a24dcf2228907c661c3d370d84d77fbe2d
StarkDroid <gamerprince.exp@gmail.com>
2022-08-17 11:14:47 +0300

    Clock: Add ability to resize QS & Statusbar Clock [1/2]
    spkal01: Utilize Tuner Settings and simplify implementation
    
    Co-Authored-By: spkal01 <kalligeross@gmail.com>
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    Change-Id: I4ca9d8fe6258b2dfa3e5ae7ab8bde2fb3372aff5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
+++ packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
@@ -511,6 +511,7 @@ constructor(
             date.setTextColor(textColorPrimary)
             qsCarrierGroup.updateColors(textColorPrimary, colorStateList)
             batteryIcon.updateColors(textColorPrimary, textColorSecondary, textColorPrimary)
+            clock.updateClockSize()
         }
     }
 
