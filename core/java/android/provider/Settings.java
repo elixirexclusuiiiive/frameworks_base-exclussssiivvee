f7153aac32b09872299b84d411428d43e60cd4ef
Vachounet <vachounet@live.fr>
2022-11-22 22:02:30 +0100

    base: Custom (User Selected) UDFPS Icon [1/2]
    SDB: forward port to A12 & adapt for new implementation of UDFPS Icon
    
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5929,6 +5929,16 @@ public final class Settings {
          * @hide
          */
         public static final String BRIGHTNESS_SLIDER_HAPTIC_ENABLED = "brightness_slider_haptic_enabled";
+         
+	/**
+	 * @hide
+         */
+        public static final String OMNI_CUSTOM_FP_ICON_ENABLED = "custom_fp_icon_enabled";
+
+        /**
+         * @hide
+         */
+        public static final String OMNI_CUSTOM_FP_ICON = "custom_fingerprint_icon";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
