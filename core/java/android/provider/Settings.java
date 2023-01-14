35ea209363bf4b7ae2e330ce52025e8133ce48b3
Ido Ben-Hur <idoybh2@gmail.com>
2023-01-14 19:56:01 +0200

    base: Allow setting bottom keyguard shortcuts [1/2]
    Allows changing the priority, sides and to disable

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6094,6 +6094,17 @@ public final class Settings {
         public static final String QS_TRANSPARENCY = "qs_transparency";
 
         /**
+         * Bottom screen shortcuts on keyguard
+         * Two lists of strings delimeted by ;
+         * Each list of string is delimited by ,
+         * Valid strings are: home, wallet, qr, camera, flashlight and none
+         * The order in each list decides the priority for each shortcut
+         * @hide
+         */
+        @Readable
+        public static final String KEYGUARD_QUICK_TOGGLES = "keyguard_quick_toggles";
+
+        /**
          * These are all public system settings
          *
          * @hide
