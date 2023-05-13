42094a32e8f6391ef6bc66136ef3a8f6c8df3670
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-05-13 12:43:38 +0530

    base: Disable 5G in battery saver mode [1/3]
    Change-Id: I90d1b171de8edf7260638d2f93e1435295551cac
    Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5808,6 +5808,12 @@ public final class Settings {
         public static final String DOZE_TRIGGER_DOUBLETAP = "doze_trigger_doubletap";
 
         /**
+         * Whether to disable 5G in battery saver mode
+         * @hide
+         */
+        public static final String LOW_POWER_DISABLE_5G = "low_power_disable_5g";
+
+        /**
          * These are all public system settings
          *
          * @hide
