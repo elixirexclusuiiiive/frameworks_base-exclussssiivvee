d263c186e5c29fa7475c2b48141ee1587933d7a5
ugly-kid-af <dwarmachine24@gmail.com>
2023-07-15 20:43:15 +0530

    Revert "SystemUI: Add quick settings pull down with one finger"
    This reverts commit 9221648e5477a3e104f4ebfcf0395203d9272b82.
    
    Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5620,13 +5620,6 @@ public final class Settings {
         public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
 
         /**
-         * Whether to allow one finger quick settings expansion on the side of the statusbar.
-         * 0 = 0ff, 1 = right, 2 = left, 3 = both
-         * @hide
-         */
-        public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "qs_quick_pulldown";
-
-        /**
          * Enable statusbar double tap gesture to put device to sleep
          * @hide
          */
