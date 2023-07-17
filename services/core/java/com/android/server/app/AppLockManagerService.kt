29ea054f20f4084afa3c1e08f6815d818e0dbec0
ugly-kid-af <dwarmachine24@gmail.com>
2023-07-17 16:19:18 +0530

    fixup applock utils
    Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>

--- services/core/java/com/android/server/app/AppLockManagerService.kt
+++ services/core/java/com/android/server/app/AppLockManagerService.kt
@@ -50,7 +50,7 @@ import android.util.Slog
 
 import com.android.internal.R
 import com.android.internal.annotations.GuardedBy
-import com.android.internal.util.proton.ProtonUtils
+import com.android.internal.util.custom.customUtils
 import com.android.server.LocalServices
 import com.android.server.SystemService
 import com.android.server.notification.NotificationManagerInternal
@@ -494,7 +494,7 @@ class AppLockManagerService(
                 userId
             )
             if (!aInfo.isSystemApp()) return
-            if (ProtonUtils.launchablePackages(context).contains(pkg)) return
+            if (customUtils.launchablePackages(context).contains(pkg)) return
             if (!whiteListedSystemApps.contains(pkg))
                 throw IllegalArgumentException("System package $pkg is not whitelisted")
         } catch(e: PackageManager.NameNotFoundException) {
