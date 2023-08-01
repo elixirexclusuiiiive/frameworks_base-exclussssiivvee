f304ea653bf5dbe9fcfff4ed8c48aae0242e9ab2
Nishant Kumar <www.rajsonu13@gmail.com>
2023-08-01 16:45:28 +0530

    Revert "fwb: Set default value of qs transparency as overlay [1/2]"
    This reverts commit ae36a74779192fd84caa82cee99092d9eb50824f.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -4203,9 +4203,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Tune
                 mNotificationInterruptStateProvider.setUseLessBoringHeadsUp(lessBoringHeadsUp);
                 break;
             case QS_TRANSPARENCY:
-	            final Resources res = mContext.getResources();
                 mScrimController.setCustomScrimAlpha(
-                        TunerService.parseInteger(newValue, res.getInteger(com.android.internal.R.integer.config_qs_transparency)));
+                        TunerService.parseInteger(newValue, 85));
                 break;
             default:
                 break;
